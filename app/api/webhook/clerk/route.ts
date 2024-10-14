50ecb00dfaff68e78e9a471bf8aa134e5f766a9e
niraj <nirajversilal@gmail.com>
2024-10-15 00:35:58 +0500

    req bug fixed

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
+  req.cookies.set("cookie", "hello");
   return NextResponse.json({ message: "hello world" });
 };
