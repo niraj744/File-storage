494b81262ac93af2a32c351f88948e4d538539f3
niraj <nirajversilal@gmail.com>
2024-10-15 00:26:09 +0500

    bug fixed

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -1,1 +1,5 @@
-export const POST = async (req: Request) => {};
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+  return NextResponse.json({ message: "hello world" });
+};
